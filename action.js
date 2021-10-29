508d3971edd65365762b19f73bdf7428ba0e1f8a
Mads-Emil Kjeldsen Hansen <mads-emilkjeldsenhansen@Mads-Emils-MacBook-Pro.local>
2021-10-29 13:34:06 +0200

    prediction works

--- action.js
+++ action.js
@@ -7,10 +7,21 @@ console.log('ml5 version:', ml5.version);
 
 function tohappy() {
     let split_text = document.getElementById("happy").value;
-    const prediction = sentiment.predict(split_text);
+    let prediction = sentiment.predict(split_text);
     text = split_text.split(" ");
-    console.log(text); 
+    console.log(text);
     console.log(prediction);
+    for (let i = 0; i < text.length; i++) {
+        prediction = sentiment.predict(text[i]);
+        console.log(prediction);
+        if (prediction > 0.5) {
+            console.log("Good word :)")
+        } else if (prediction < 0.5) {
+            console.log("Bad word :'(");
+        } else {
+            console.log("Unpredicted outcome");
+        }
+    }
 }
 
 function place() {
